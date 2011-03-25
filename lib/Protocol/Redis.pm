f6426f8d7b70f5401b371b50fc12cddc65ffe3b4
Сергей Засенко <undef@cpan.org>
2011-03-25 13:42:57 +0000

    Import of UNDEF/Protocol-Redis-0.9001 from CPAN.
    gitpan-cpan-distribution: Protocol-Redis
    gitpan-cpan-version:      0.9001
    gitpan-cpan-path:         UNDEF/Protocol-Redis-0.9001.tar.gz
    gitpan-cpan-author:       UNDEF
    gitpan-cpan-maturity:     released

--- lib/Protocol/Redis.pm
+++ lib/Protocol/Redis.pm
@@ -3,10 +3,9 @@ package Protocol::Redis;
 use strict;
 use warnings;
 
-use List::Util ();
 require Carp;
 
-our $VERSION = 0.9;
+our $VERSION = 0.9001;
 
 sub new {
     my $class = shift;
@@ -15,19 +14,72 @@ sub new {
     my $self = {@_};
     bless $self, $class;
 
-    $self->newline($self->{newline});
     $self->on_message($self->{on_message});
     $self->{_messages} = [];
-    $self->_change_state(\&_state_new_message);
+
+    $self->{_state} = \&_state_new_message;
 
     $self;
 }
 
-sub newline {
-    my ($self, $newline) = @_;
+sub use_api {
+    my ($self, $api_version) = @_;
 
-    $self->{_newline} = $newline or "\r\n";
+    return $api_version == 1;
 }
+
+my %message_type_encoders = (
+    '+' => \&_encode_string,
+    '-' => \&_encode_string,
+    ':' => \&_encode_string,
+    '$' => \&_encode_bulk,
+    '*' => \&_encode_multi_bulk,
+);
+
+sub encode {
+    my ($self, $message) = @_;
+
+    if (my $encoder = $message_type_encoders{$message->{type}}) {
+        $encoder->($self, $message);
+    }
+    else {
+        Carp::croak(qq/Unknown message type $message->{type}/);
+    }
+}
+
+sub _encode_string {
+    my ($self, $message) = @_;
+
+    $message->{type} . $message->{data} . "\r\n";
+}
+
+sub _encode_bulk {
+    my ($self, $message) = @_;
+
+    my $data = $message->{data};
+
+    return '$-1' . "\r\n"
+      unless defined $data;
+
+    '$' . length($data) . "\r\n" . $data . "\r\n";
+}
+
+sub _encode_multi_bulk {
+    my ($self, $message) = @_;
+
+    my $data = $message->{data};
+
+    return '*-1' . "\r\n"
+      unless defined $data;
+
+    my $e_message = '*' . scalar(@$data) . "\r\n";
+    foreach my $element (@$data) {
+        $e_message .= $self->encode($element);
+    }
+
+    $e_message;
+}
+
 
 sub get_message {
     my ($self) = @_;
@@ -59,16 +111,7 @@ sub parse {
     my ($self, $chunk) = @_;
 
     # Just pass chunk to current vertex
-    $self->{_state}->($self, $chunk);
-}
-
-sub _change_state {
-    my ($self, $new_state, $chunk) = @_;
-
-    $self->{_state} = $new_state;
-
-    # Pass rest of chunk to new vertex
-    $new_state->($self, $chunk) if $chunk;
+    1 while $chunk = $self->{_state}->($self, $chunk);
 }
 
 sub _message_parsed {
@@ -82,7 +125,34 @@ sub _message_parsed {
     else {
         push @{$self->{_messages}}, $message;
     }
-    $self->_change_state(\&_state_new_message, $chunk);
+
+    $self->{_state} = \&_state_new_message;
+    $chunk;
+}
+
+my %message_type_parsers = (
+    '+' => \&_state_string_message,
+    '-' => \&_state_string_message,
+    ':' => \&_state_string_message,
+    '$' => \&_state_bulk_message,
+    '*' => \&_state_multibulk_message,
+);
+
+sub _state_parse_message_type {
+    my ($self, $chunk) = @_;
+
+    my $cmd = substr $chunk, 0, 1, '';
+
+    if ($cmd) {
+        if (my $parser = $message_type_parsers{$cmd}) {
+            $self->{_cmd}{type} = $cmd;
+            $self->{_state} = $parser;
+            return $chunk;
+        }
+        else {
+            Carp::croak(qq/Unexpected input "$cmd"/);
+        }
+    }
 }
 
 sub _state_new_message {
@@ -90,47 +160,29 @@ sub _state_new_message {
 
     $self->{_cmd} = {type => undef, data => undef};
 
-    my $cmd = substr $chunk, 0, 1, '';
+    $self->{_state_cb} = \&_message_parsed;
 
-    if (List::Util::first { $cmd eq $_ } (qw/+ - :/)) {
-        $self->{_cmd}{type} = $cmd;
-        $self->{_state_cb} = \&_message_parsed;
-        $self->_change_state(\&_state_string_message, $chunk);
-    }
-    elsif ($cmd eq '$') {
-        $self->{_cmd}{type} = $cmd;
-        $self->{_state_cb} = \&_message_parsed;
-        $self->_change_state(\&_state_bulk_message, $cmd . $chunk);
-    }
-    elsif ($cmd eq '*') {
-        $self->{_cmd}{type} = $cmd;
-        $self->{_state_cb} = \&_message_parsed;
-        $self->_change_state(\&_state_multibulk_message, $chunk);
-
-    }
-    else {
-        Carp::croak(qq/Unexpected input "$cmd"/);
-    }
+    $self->{_state} = \&_state_parse_message_type;
+    $chunk;
 }
 
 sub _state_string_message {
     my ($self, $chunk) = @_;
 
     my $str = $self->{_state_string} .= $chunk;
-    my $i = index $str, $self->newline;
+    my $i = index $str, "\r\n";
 
     # string isn't full
     return if $i < 0;
 
     # We got full string
-    my $result = substr $str, 0, $i, '';
+    $self->{_cmd}{data} = substr $str, 0, $i, '';
 
     # Delete newline
-    substr $str, 0, length($self->newline), '';
+    substr $str, 0, 2, '';
 
     delete $self->{_state_string};
 
-    $self->{_cmd}{data} = $result;
     $self->{_state_cb}->($self, $str);
 }
 
@@ -145,9 +197,6 @@ sub _state_bulk_message {
 
         $self->{_bulk_size} = delete $self->{_cmd}{data};
 
-        # Delete starting '$'
-        substr $self->{_bulk_size}, 0, 1, "";
-
         if ($self->{_bulk_size} == -1) {
 
             # Nil
@@ -156,28 +205,31 @@ sub _state_bulk_message {
         }
         else {
             $self->{_state_cb} = $bulk_state_cb;
-            $self->_change_state(\&_state_bulk_message_data, $chunk);
+            $self->{_state}    = \&_state_bulk_message_data;
+            $chunk;
         }
     };
-    $self->_change_state(\&_state_string_message, $chunk);
+    $self->{_state} = \&_state_string_message;
+    $chunk;
 }
 
 sub _state_bulk_message_data {
     my ($self, $chunk) = @_;
 
     my $str = $self->{_state_string} .= $chunk;
-    if (length $str >= $self->{_bulk_size} + length $self->newline) {
-        my $result = substr $str, 0, $self->{_bulk_size}, '';
 
-        # Delete ending newline
-        substr $str, 0, length $self->newline, '';
+    # String + newline parsed
+    return unless length $str >= $self->{_bulk_size} + 2;
 
-        delete $self->{_state_string};
-        delete $self->{_bulk_size};
+    $self->{_cmd}{data} = substr $str, 0, $self->{_bulk_size}, '';
 
-        $self->{_cmd}{data} = $result;
-        $self->{_state_cb}->($self, $str);
-    }
+    # Delete ending newline
+    substr $str, 0, 2, '';
+
+    delete $self->{_state_string};
+    delete $self->{_bulk_size};
+
+    $self->{_state_cb}->($self, $str);
 }
 
 sub _state_multibulk_message {
@@ -187,19 +239,24 @@ sub _state_multibulk_message {
     my $data           = [];
     my $mbulk_process;
 
+    my $arguments_num;
+
     $mbulk_process = sub {
         my ($self, $chunk) = @_;
 
-        push @$data, delete $self->{_cmd}{data};
+        push @$data,
+          { type => delete $self->{_cmd}{type},
+            data => delete $self->{_cmd}{data}
+          };
 
-        if (scalar @$data == $self->{_mbulk_arg_num}) {
+        if (scalar @$data == $arguments_num) {
 
             # Cleanup
             $mbulk_process = undef;
-            delete $self->{_mbulk_arg_num};
             delete $self->{_state_cb};
 
             # Return message
+            $self->{_cmd}{type} = '*';
             $self->{_cmd}{data} = $data;
             $mbulk_state_cb->($self, $chunk);
         }
@@ -207,7 +264,8 @@ sub _state_multibulk_message {
 
             # read next string
             $self->{_state_cb} = $mbulk_process;
-            $self->_change_state(\&_state_bulk_message, $chunk);
+            $self->{_state}    = \&_state_parse_message_type;
+            $chunk;
         }
     };
 
@@ -215,8 +273,8 @@ sub _state_multibulk_message {
         my ($self, $chunk) = @_;
 
         # Number of Multi-Bulk message
-        my $num = $self->{_mbulk_arg_num} = delete $self->{_cmd}{data};
-        if ($num < 1) {
+        $arguments_num = delete $self->{_cmd}{data};
+        if ($arguments_num < 1) {
             $mbulk_process = undef;
             $self->{_cmd}{data} = [];
             $mbulk_state_cb->($self, $chunk);
@@ -225,12 +283,14 @@ sub _state_multibulk_message {
 
             # We got messages
             $self->{_state_cb} = $mbulk_process;
-            $self->_change_state(\&_state_bulk_message, $chunk);
+            $self->{_state}    = \&_state_parse_message_type;
+            $chunk;
         }
     };
 
     # Get number of messages
-    $self->_change_state(\&_state_string_message, $chunk);
+    $self->{_state} = \&_state_string_message;
+    $chunk;
 }
 
 1;
@@ -238,13 +298,56 @@ __END__
 
 =head1 NAME
 
-Protocol::Redis - Redis protocol parser
+Protocol::Redis - Redis protocol parser/encoder with asynchronous capabilities.
+
+=head1 SYNOPSIS
+
+    use Protocol::Redis;
+    my $redis = Protocol::Redis->new;
+
+    # Init used API version
+    $redis->use_api(1) or die "API v1 not supported";
+
+    $redis->parse("+foo\r\n");
+
+    # get parsed message
+    my $message = $redis->get_message;
+    print "parsed message: ", $message->{data}, "\n";
+
+    # asynchronous parsing interface
+    $redis->on_message(sub {
+        my ($redis, $message) = @_;
+        print "parsed message: ", $message->{data}, "\n";
+    });
+
+    # parse pipelined message
+    $redis->parse("+bar\r\n-error\r\n");
+
+    # create message
+    print "Get key message:\n",
+      $redis->encode({type => '*', data => [
+         {type => '$', data => 'string'},
+         {type => '+', data => 'OK'}
+    ]});
 
 =head1 DESCRIPTION
 
-Redis protocol parser.
+Redis protocol parser/encoder with asynchronous capabilities and L<pipelining|http://redis.io/topics/pipelining> support.
 
-=head1 METHODS
+=head1 APIv1 (DRAFT)
+
+Protocol::Redis APIv1 uses
+"L<Unified Request Protocol|http://redis.io/topics/protocol>" for message
+encoding/parsing and supports methods described further. Client libraries
+should call $redis->use_api(1) to start using APIv1.
+
+=head2 C<use_api>
+
+    $redis->use_api(1) or die "API v1 not supported";
+
+Tell Protocol::Redis to use specific API version. Return false if API version
+not supported. Client libraries should call this method first and check
+returned value.
 
 =head2 C<parse>
 
@@ -269,11 +372,20 @@ Get parsed message or undef.
 
 Calls callback on each parsed message.
 
+=head2 C<encode>
+    
+    my $string = $redis->encode({type => '+', data => 'test'});
+    $string = $redis->encode(
+        {type => '*', data => [
+            {type => '$', data => 'test'}]});
+
+Encode data into redis message.
+
 =head1 SUPPORT
 
 =head2 IRC
 
-    #ru.pm on irc.perl.org
+    #redis on irc.perl.org
     
 =head1 DEVELOPMENT
 
@@ -285,7 +397,13 @@ Calls callback on each parsed message.
 
 Sergey Zasenko, C<undef@cpan.org>.
 
-=head1 COPYRIGHT (C) 2011, Sergey Zasenko.
+=head1 CREDITS
+
+David Leadbeater (dgl)
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2011, Sergey Zasenko.
 
 This program is free software, you can redistribute it and/or modify it under
 the same terms as Perl 5.10.
